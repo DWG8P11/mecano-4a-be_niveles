7920e3c2ee9024cc9dcf7a2ba12ee6f9dc63865c
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-11-26 10:44:56 -0500

    Advises agregadas

--- niveles_ms/src/main/java/com/lanebulosadeqwerty/exceptions/NivelNoEncontradoException.java
+++ niveles_ms/src/main/java/com/lanebulosadeqwerty/exceptions/NivelNoEncontradoException.java
@@ -3,6 +3,8 @@ package com.lanebulosadeqwerty.exceptions;
 /*
  * Al agregar o modificar una LECCION O PUNTAJE, poniendo un número de nivel que no esté registrado
  */
-public class NivelNoEncontradoException {
-    
+public class NivelNoEncontradoException extends RuntimeException {
+    public NivelNoEncontradoException(String message) {
+        super(message);
+    }
 }
--- niveles_ms/src/main/java/com/lanebulosadeqwerty/exceptions/NivelYaExisteAdvice.java
+++ niveles_ms/src/main/java/com/lanebulosadeqwerty/exceptions/NivelYaExisteAdvice.java
@@ -1,5 +1,19 @@
 package com.lanebulosadeqwerty.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+@ResponseBody
 public class NivelYaExisteAdvice {
-    
+    @ResponseBody
+    @ExceptionHandler(NivelYaExisteException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    String NivelYaExisteAdvice(NivelYaExisteException ex){
+        return ex.getMessage();
+    }
 }
